4f6b64f563821ca50e4f727ed44bdd64e48c07c9
Giovanna Mendes de Sousa Rodrigues <48605558+giovannamsr@users.noreply.github.com>
2019-05-06 18:15:48 -0300

    Add files via upload

---
+++ Assembler-master/Add.asm
@@ -0,0 +1,10 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// Computes R0 = 2 + 3
+
+@2
+D=A
+@3
+D=D+A
+@0
+M=D
---
+++ Assembler-master/Main.py
@@ -0,0 +1,28 @@
+import Parser, Assembler
+#read in .asm
+#parse file
+#output .hack
+
+def main():
+    askForFile = True
+    while (askForFile):
+        try:
+            fileName = input("Please enter file name to read: ")
+            asmFile = open(fileName, 'r')
+            askForFile = False
+            FileParser = Parser.Parser(asmFile)
+            #close file
+            asmFile.close()
+
+            #list of commands to be interpreted
+            rawCommands = FileParser.getContents
+
+            HackAssembler = Assembler.Assembler(rawCommands, fileName)
+            HackAssembler.addUserSymbols()
+            HackAssembler.translate()
+            HackAssembler.outputFile()
+        except:
+            print("Error, no such file")
+
+if __name__ == '__main__': 
+    main()
